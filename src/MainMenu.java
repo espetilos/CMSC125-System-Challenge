5c143b4533b34ca28f53631b4948c24cc2d7ceb8
kpeclipse@up.edu.ph <kpeclipse@up.edu.ph>
2023-02-02 16:33:53 +0800

    GB-55: Add Game Bundle Label

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,8 +1,11 @@
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -14,6 +17,9 @@ public class MainMenu extends JPanel {
     private Window window;
     private JButton creditsButton;
     private JButton exitButton;
+    private JLabel gameBundleLabel;
+
+    private Font emulogicFont;
 
     public MainMenu(int width, int height, String panel, Window w) {
         setSize(width, height);
@@ -21,31 +27,42 @@ public class MainMenu extends JPanel {
         setOpaque(true);
 
         window = w;
+        emulogicFont = window.useFont(System.getProperty("user.dir") + "/src/resources/Emulogic.ttf", 20);
 
         if (panel == "gameBundleMain") { // If showing the Game Bundle Main Menu Panel
-            setButtons();
             setBackground(Color.BLACK);
+            setButtons();
+            gameBundleLabel = label("System Challenge", emulogicFont, Color.WHITE);
+            gameBundleLabel.setBounds(430, 50, 320, 50);
+            add(gameBundleLabel);
         }
 
         setActionAndMouseListeners();
     }
 
-    public JButton button(String text) {
+    private JButton button(String text, Font font, Color color) {
         JButton theButton = new JButton(text);
-        theButton.setForeground(Color.WHITE);
+        theButton.setForeground(color);
+        theButton.setFont(font);
         theButton.setContentAreaFilled(false);
         theButton.setFocusPainted(false);
         theButton.setBorder(BorderFactory.createEmptyBorder());
         return theButton;
     }
 
+    private JLabel label(String label, Font font, Color color) {
+        JLabel theLabel = new JLabel(label);
+        theLabel.setFont(font);
+        theLabel.setForeground(color);
+        theLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        return theLabel;
+    }
+
     public void setButtons() {
-        creditsButton = button("Credits");
-        creditsButton.setFont(window.useFont(System.getProperty("user.dir") + "/src/resources/Emulogic.ttf", 20));
+        creditsButton = button("Credits", emulogicFont, Color.WHITE);
         creditsButton.setBounds(450, 550, 300, 50);
 
-        exitButton = button("Exit");
-        exitButton.setFont(window.useFont(System.getProperty("user.dir") + "/src/resources/Emulogic.ttf", 20));
+        exitButton = button("Exit", emulogicFont, Color.WHITE);
         exitButton.setBounds(450, 600, 300, 50);
 
         add(creditsButton);
